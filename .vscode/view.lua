79c05e384f33055bb0c33c886796a51b5a9c6d29
狸猫呐 <111165793+limao996@users.noreply.github.com>
2023-08-30 17:34:00 +0800

    Delete .vscode directory

--- .vscode/view.lua
+++
@@ -1,9 +0,0 @@
-require "import"
-import "android.app.*"
-import "android.os.*"
-import "android.widget.*"
-import "android.view.*"
-
-activity.setTheme(android.R.style.Theme_Material)
-activity.setTitle("视图预览")
-activity.setContentView("layout")
